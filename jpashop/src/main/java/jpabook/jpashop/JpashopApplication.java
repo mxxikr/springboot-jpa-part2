7e2e10e8c294b8574c0866393283ea822f9c3942
mxxikr <mxxikr@gmail.com>
2026-02-18 17:56:34 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화: 간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,9 +1,7 @@
 package jpabook.jpashop;
 
-import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
