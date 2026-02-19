022c99777fbed9973a42247a73da1fb5c9e95488
mxxikr <mxxikr@gmail.com>
2026-02-19 14:46:43 +0900

    API 개발 고급 - 컬렉션 조회 최적화: 주문 조회 V1: 엔티티 직접 노출

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate7.Hibernate7Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,11 +12,10 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
-//	@Bean
-//	Hibernate5JakartaModule hibernate5Module() {
-//		Hibernate5JakartaModule hibernate5Module = new Hibernate5JakartaModule();
-//		//강제 지연 로딩 설정
+	@Bean
+	Hibernate7Module hibernate7Module() {
+//		강제 지연 로딩 설정
 //		hibernate5Module.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
-//		return hibernate5Module;
-//	}
+		return new Hibernate7Module();
+	}
 }
